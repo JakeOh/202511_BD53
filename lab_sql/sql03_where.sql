8c368670c5d786a72ff689b17928e90d7691f870
JakeOh <jake.oh.lecture@gmail.com>
2025-11-20 15:30:14 +0900

    where 조건절 연습

--- lab_sql/sql03_where.sql
+++ lab_sql/sql03_where.sql
@@ -66,3 +66,60 @@ order by sal desc;
 -- 수당은 null이 아니고, 급여는 1500 미만인 직원들
 select * from emp 
 where comm is not null and sal < 1500;
+
+-- 10번 또는 20번 부서에서 근무하는 직원들의 부서번호, 이름, 급여를 출력.
+-- 정렬: (1) 부서번호 오름차순, (2) 급여 내림차순.
+select deptno, ename, sal
+from emp
+where deptno = 10 or deptno = 20
+order by deptno, sal desc;
+
+select deptno, ename, sal
+from emp
+where deptno in (10, 20)
+order by deptno, sal desc;
+
+-- 업무가 'CLERK'인 직원들의 이름, 업무, 급여를 출력. 이름순으로 출력.
+select ename, job, sal
+from emp
+where job = 'CLERK'
+order by ename;
+--> (주의) SQL 식별자(예약어, 테이블 이름, 컬럼 이름)를 제외한 문자열은 작은따옴표('')를 사용해야 함.
+-- 문자열 비교는 대/소문자를 구분함.
+-- 식별자들은 대/소문자를 구분하지 않음.
+
+-- 업무가 'CLERK' 또는 'MANAGER'인 직원들의 이름, 업무, 급여를 출력.
+-- 정렬: (1) 업무, (2) 급여
+select ename, job, sal
+from emp
+where job = 'CLERK' or job = 'MANAGER'
+order by job, sal;
+
+select ename, job, sal
+from emp
+where job in ('CLERK', 'MANAGER')
+order by job, sal;
+
+-- 업무가 영업사원, 관리자, 분석가인 직원들.
+select * from emp
+where job = 'SALESMAN' or job = 'MANAGER' or job = 'ANALYST'
+order by job;
+
+select * from emp
+where job in ('SALESMAN', 'MANAGER', 'ANALYST')
+order by job;
+
+-- 20번 부서에서 근무하는 'CLERK'의 모든 레코드(모든 컬럼)를 출력.
+select * from emp
+where deptno = 20 and job = 'CLERK';
+
+-- CLERK, ANALYST, MANAGER가 아닌 직원들의 사번, 이름, 업무, 급여를 사번순 출력.
+select empno, ename, job, sal
+from emp
+where job != 'CLERK' and job != 'ANALYST' and job != 'MANAGER'
+order by empno;
+
+select empno, ename, job, sal
+from emp
+where job not in ('CLERK', 'ANALYST', 'MANAGER')
+order by empno;
