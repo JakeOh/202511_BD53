44f48cad481a99ee9c5b1be5f934f23672a16f91
JakeOh <jake.oh.lecture@gmail.com>
2025-11-26 14:21:16 +0900

    HR 연습문제

--- lab_sql/sql09_exercises.sql
+++ lab_sql/sql09_exercises.sql
@@ -79,12 +79,107 @@ from employees e
     join departments d on e.department_id = d.department_id
 ;
 
+select 
+    e.first_name || ' ' || e.last_name as "직원이름",
+    d.department_name
+from employees e, departments d 
+where e.department_id = d.department_id
+;
+
 -- 2. 직원의 이름과 부서 이름을 출력. 부서 번호가 없는 직원도 출력.
+select 
+    e.first_name || ' ' || e.last_name as "직원이름",
+    d.department_name
+from employees e
+    left join departments d on e.department_id = d.department_id
+;
+
+select 
+    e.first_name || ' ' || e.last_name as "직원이름",
+    d.department_name
+from employees e, departments d 
+where e.department_id = d.department_id(+)
+;
+
 -- 3. 직원의 이름과 직무 이름(job title)을 출력.
+select
+    e.first_name || ' ' || e.last_name as "직원이름",
+    j.job_title
+from employees e
+    join jobs j on e.job_id = j.job_id;
+
+select
+    e.first_name || ' ' || e.last_name as "직원이름",
+    j.job_title
+from employees e, jobs j 
+where e.job_id = j.job_id;
+
 -- 4. 직원의 이름과 직원이 근무하는 도시 이름(city)를 출력.
+select
+    e.first_name || ' ' || e.last_name as "직원이름",
+    l.city
+from employees e
+    join departments d on e.department_id = d.department_id
+    join locations l on d.location_id = l.location_id;
+
+select
+    e.first_name || ' ' || e.last_name as "직원이름",
+    l.city
+from employees e, departments d, locations l
+where e.department_id = d.department_id
+    and d.location_id = l.location_id;
+
 -- 5. 직원의 이름과 직원이 근무하는 도시 이름(city)를 출력. 근무 도시를 알 수 없는 직원도 출력.
+select
+    e.first_name || ' ' || e.last_name as "직원이름",
+    l.city
+from employees e
+    left join departments d on e.department_id = d.department_id
+    left join locations l on d.location_id = l.location_id;
+
+select
+    e.first_name || ' ' || e.last_name as "직원이름",
+    l.city
+from employees e, departments d, locations l
+where e.department_id = d.department_id(+)
+    and d.location_id = l.location_id(+);
+
 -- 6. 2008년에 입사한 직원들의 이름을 출력.
+-- 입사일을 DATE 타입의 크기 비교를 사용
+select
+    first_name || ' ' || last_name as "직원이름"
+from employees
+where hire_date between to_date('2008-01-01', 'YYYY-MM-DD') and to_date('2008-12-31', 'YYYY-MM-DD');
+
+select
+    first_name || ' ' || last_name as "직원이름"
+from employees
+where hire_date between '2008-01-01' and '2008-12-31';
+--> '2008-01-01' 문자열을 암묵적으로 DATE 타입으로 변환한 다음에 크기 비교.
+
+-- 입사일(DATE 타입)을 문자열로 변환해서 문자열 비교.
+select
+    first_name || ' ' || last_name as "직원이름"
+from employees
+where to_char(hire_date, 'YYYY') = '2008';
+
 -- 7. 2008년에 입사한 직원들의 부서 이름과 부서별 인원수 출력.
+select
+    d.department_name, count(*) as "직원수"
+from employees e
+    join departments d on e.department_id = d.department_id
+where to_char(e.hire_date, 'YYYY') = '2008'
+group by d.department_name
+;
+
+select
+    d.department_name, count(*) as "직원수"
+from employees e, departments d
+where e.department_id = d.department_id
+    and to_char(e.hire_date, 'YYYY') = '2008'
+group by d.department_name;
+
+
 -- 8. 2008년에 입사한 직원들의 부서 이름과 부서별 인원수 출력. 
 --    단, 부서별 인원수가 5명 이상인 경우만 출력.
 -- 9. 부서번호, 부서별 급여 평균을 검색. 소숫점 한자리까지 반올림 출력.
