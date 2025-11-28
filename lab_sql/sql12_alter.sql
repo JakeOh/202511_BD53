46751853fb3e4aacab7ab91dc1afcd78ddc8f254
JakeOh <jake.oh.lecture@gmail.com>
2025-11-28 12:15:59 +0900

    alter table

---
+++ lab_sql/sql12_alter.sql
@@ -0,0 +1,7 @@
+/*
+ * alter table: 테이블 (정의) 변경.
+ * 1. 이름 변경: alter table ... rename ... to ...;
+ * 2. 추가: alter table ... add ...;
+ * 3. 삭제: alter table ... drop ...;
+ * 4. 수정: alter table ... modify ...;
+ */
