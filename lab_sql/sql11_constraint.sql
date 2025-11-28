a39a8ef8c5e9d631f6f6daa02971141daf36cd97
JakeOh <jake.oh.lecture@gmail.com>
2025-11-28 12:15:40 +0900

    create table &  제약조건 연습

--- lab_sql/sql11_constraint.sql
+++ lab_sql/sql11_constraint.sql
@@ -173,7 +173,18 @@ select * from ex_test;
  * (4) cust_gender: 고객 성별. 1자리 정수. 기본값 0. (0, 1, 2) 중 1개 값만 가능.
  * (5) cust_age: 고객 나이. 3자리 정수. 기본값 0. 0 이상 200 이하의 정수만 가능.
  */
-
+create table customers (
+    cust_id     varchar2(20)
+                constraint customers_pk primary key
+                constraint customers_ck_cust_id check (lengthb(cust_id) >= 8),
+    cust_pw     varchar2(20) not null
+                constraint customers_ck_cust_pw check (lengthb(cust_pw) >= 8),
+    cust_email  varchar2(100),
+    cust_gender number(1) default 0
+                constraint customers_ck_cust_gender check (cust_gender in (0, 1, 2)),
+    cust_age    number(3) default 0
+                constraint customers_ck_cust_age check (cust_age between 0 and 200)
+);
 
 /*
  * 연습문제 2.
@@ -181,7 +192,17 @@ select * from ex_test;
  * 컬럼:
  * (1) order_id: 주문번호. 10자리 정수. primary key.
  * (2) order_date: 주문 날짜. 기본값은 현재 시간.
- * (3) order_method: 주문 방법. 8 byte 문자열. ('online', 'offline') 중 1개 값만 가능.
- * (4) cust_id: 주문 고객 아이디. 20 byte 문자열. not null. customers(cust_id)를 참조.
+ * (3) order_method: 주문 방법. 최대 8 byte 문자열. ('online', 'offline') 중 1개 값만 가능.
+ * (4) cust_id: 주문 고객 아이디. 최대 20 byte 문자열. not null. customers(cust_id)를 참조.
  */
-
+ create table orders (
+    order_id        number(10),
+    order_date      date 
+                    default sysdate,
+    order_method    varchar2(8),
+    cust_id         varchar2(20) not null,
+    
+    constraint orders_pk primary key (order_id),
+    constraint orders_ck_order_method check (order_method in ('online', 'offline')),
+    constraint orders_fk_cust_id foreign key (cust_id) references customers (cust_id)
+ );
