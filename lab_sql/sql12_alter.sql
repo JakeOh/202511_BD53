05e394d74616261f191bfea4a82d3c64c4adb2c3
JakeOh <jake.oh.lecture@gmail.com>
2025-11-28 15:32:43 +0900

    alter table drop column/constraint

--- lab_sql/sql12_alter.sql
+++ lab_sql/sql12_alter.sql
@@ -46,3 +46,42 @@ commit;
 
 -- id 컬럼은 null이 없고, 중복되는 값들도 없음. -> PK 제약조건을 줄 수 있음.
 alter table students add constraint students_pk primary key (id);
+
+
+-- 3. 삭제(drop): 컬럼 삭제, 제약조건 삭제.
+-- (1) 컬럼 삭제
+-- ALTER TABLE 테이블_이름 DROP COLUMN 컬럼_이름;
+-- students 테이블에서 birthday 컬럼을 삭제
+alter table students drop column birthday;
+
+-- (2) 제약조건 삭제
+-- ALTER TABLE 테이블_이름 DROP CONSTRAINT 제약조건_이름;
+-- students 테이블에서 제약조건 students_pk를 삭제
+alter table students drop constraint students_pk;
+
+
+-- (참고) 메타 테이블: 테이블을 관리하기 위한 테이블. (예) user_tables, user_constraints
+select * from user_tables;
+--> 현재 접속 계정(scott)에서 생성한 테이블들.
+
+select table_name from user_tables;
+
+-- 'ex_'로 시작하는 모든 테이블 이름들을 찾아보세요.
+select table_name from user_tables
+where lower(table_name) like 'ex_%';
+-- where upper(table_name) like 'EX_%';
+-- where table_name like 'EX_%';
+--> 오라클은 테이블 이름/컬럼 이름/제약조건 이름들을 메타 테이블에 저장할 때 대문자로만 저장.
+--> 메타 테이블에서 테이블 이름 또는 제약조건 이름을 찾고 싶을 때는 대문자 비교를 사용.
+
+-- 제약조건을 관리하는 메타 테이블
+select * from user_constraints;
+
+-- ex_tbl1 테이블에 설정된 제약조건 이름, 타입, 내용(search_condition)을 출력
+select constraint_name, constraint_type, search_condition
+from user_constraints
+where table_name = 'EX_TBL1';
+
+-- 'SYS_'로 시작하는 제약조건 이름들을 찾으세요.
+select constraint_name from user_constraints
+where constraint_name like 'SYS_%';
