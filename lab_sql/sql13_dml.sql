66001718e95dca6bdcacc6a262f3d66bb8e8eba2
JakeOh <jake.oh.lecture@gmail.com>
2025-12-01 10:22:45 +0900

    update

--- lab_sql/sql13_dml.sql
+++ lab_sql/sql13_dml.sql
@@ -16,10 +16,45 @@ values ('오쌤', 200);
 
 -- emp 테이블에서 comm이 null이 아닌 직원들의 이름, 업무, 급여, 수당을 bonus 테이블에 삽입.
 insert into bonus /* (ename, job, sal, comm) */
-select job, ename, empno, deptno
+select ename, job, sal, comm
 from emp
 where comm is not null;
 
 commit;
 
 select * from bonus;
+
+
+-- update 문장: 테이블의 특정 컬럼(들)의 값을 수정(업데이트)
+-- (문법) UPDATE 테이블이름 SET 컬럼이름 = 값, ... [WHERE 조건절];
+-- where 절은 생략 가능. where 절을 생략하면 테이블의 모든 행들이 업데이트됨.
+
+select * from emp;
+
+update emp set job = 'CLERK';
+--> where 절이 없는 경우, 테이블의 모든 행(15개 행)의 job 컬럼 값들이 업데이트!
+
+-- 직전(마지막) commit 상태로 되돌림.
+rollback;
+
+
+-- 사번이 1004인 직원의 업무를 'MANAGER', 입사일을 오늘날짜로 업데이트.
+update emp
+set job ='MANAGER', hiredate = sysdate
+where empno = 1004;
+
+commit;
+--> 변경 내용 저장.
+
+rollback;
+--> rollback을 하더라도 이미 commit된 내용을 되돌릴 수는 없음.
+
+-- 사번이 7369인 직원의 급여를 1000, 수당을 100으로 업데이트.
+update emp
+set sal = 1000, comm = 100
+where empno = 7369;
+
+-- 업무가 'CLERK'인 직원들의 급여를 10% 인상.
+update emp
+set sal = sal * 1.1
+where job = 'CLERK';
