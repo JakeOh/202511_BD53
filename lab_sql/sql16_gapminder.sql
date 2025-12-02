b9dcb898c6732e1bca6e3f8c1f8ebce07c690f19
JakeOh <jake.oh.lecture@gmail.com>
2025-12-02 11:18:13 +0900

    gapminder 데이터 임포트

---
+++ lab_sql/sql16_gapminder.sql
@@ -0,0 +1,6 @@
+/*
+ * github의 gapminder.tsv 파일 다운로드, 테이블에 데이터 임포트.
+ * TSV(tab-separated values): 값들이 탭으로 구분된 텍스트 파일. csv 파일의 일종.
+ * 테이블 이름: gapminder
+ * 컬럼 이름: country, continent, year, life_exp(기대수명), pop(인구수), gdp_percap(1인당 GDP)
+ */
